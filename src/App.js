cb2523e88dd41992e77e308159b31c17865a4383
Anna Pushkareva <smirnova_spb@mail.ru>
2022-09-06 15:57:19 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
+import './assets/styles/styles.css'
+import './assets/styles/normalize.css'
+import React, {} from 'react';
+import Converter from './assets/components/Converter';
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Converter></Converter>
     </div>
   );
 }
